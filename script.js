26fdab87362e5365d82bab2da422c6652b1d8ff2
kadyjapb <163013058+kadyjapb@users.noreply.github.com>
2024-10-11 11:23:37 -0300

    criei a lista de arrays

---
+++ script.js
@@ -0,0 +1,109 @@
+function exercicio1(array) {
+
+    let soma = 0;
+
+    for (let contador = 0; contador < array.length; contador++) {
+
+        soma = soma + array[contador]
+
+    }
+
+    return soma;
+
+
+
+}
+
+function exercicio2(lista_numeros) {
+
+    let numero_maior = lista_numeros[0];
+
+    for (let contador = 0; contador < lista_numeros.length; contador++) {
+
+        if (lista_numeros[contador] > numero_maior) {
+
+            numero_maior = lista_numeros[contador];
+
+        }
+
+    }
+
+    return numero_maior;
+
+}
+
+function exercicio3(lista_numeros) {
+
+    let quantidade = 0;
+
+    for (let contador = 0; contador < lista_numeros.length; contador++) {
+
+        if (lista_numeros[contador] % 2 == 0) {
+
+            quantidade++;
+
+        }
+
+    }
+
+    return quantidade;
+
+}
+
+function exercicio4(lista_numero) {
+
+    let media = 0;
+
+    let soma = 0;
+
+    for (let contador = 0; contador < lista_numero.length; contador++) {
+
+        soma = (soma + lista_numero[contador])
+
+    }
+
+    media = soma / lista_numero.length
+
+    return media;
+
+}
+
+function exercicio5(lista_numero) {
+
+    let quantidade_positivo = 0
+
+    let quantidade_negativo = 0
+
+    for (let contador = 0; contador < lista_numero.length; contador++) {
+
+        if (lista_numero[contador] >= 0) {
+
+            quantidade_positivo++
+
+        } else {
+
+            quantidade_negativo++
+
+        }
+
+    }
+
+    console.log(`quantidade negativos ${quantidade_negativo}, quantidade positivos ${quantidade_positivo}`)
+
+    return quantidade_negativo, quantidade_positivo
+
+}
+
+function exercicio6(lista_numero, numero_multiplicador) {
+
+
+
+    for (let contador = 0; contador < lista_numero.length; contador++) {
+
+        lista_numero[contador] = lista_numero[contador] * numero_multiplicador
+
+    }
+
+    return lista_numero
+
+}
